e144477b4a239b8afb4b804387b9317fdeb2dbe0
Li Qi <liqi@>
2018-11-14 10:15:54 +0800

    add IP filtering function in logon page

--- src/app/welcome/login.component.ts
+++ src/app/welcome/login.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { SharedService } from '../shared.service';
 
+declare var $: any;
 
 @Component({
   templateUrl: 'login.component.html'
@@ -43,7 +44,26 @@ export class LoginComponent {
     console.log("reqContent:" + this.reqContent.data);
 
     this._sharedService.makeRequest('POST', '/api/login', this.reqContent).then((data: any) => {
-      alert("登录成功");
+      // IP filtering
+      $.getJSON('https://ipapi.co/json/', function(ipdata) {
+        if (data.orgid == '3461000314' &&
+           data.roles[0] != '清道夫' && 
+           data.roles[0] != '校长' &&
+           ipdata.ip != '219.145.102.230' &&
+           ipdata.ip != '117.35.48.38' ) {
+            this._sharedService.makeRequest('GET', '/api/logout', '').then((data: any) => {
+              alert("您的账号无法在校外登陆系统。IP地址:" + ipdata.ip);
+              this._sharedService.userName = '';
+              this._sharedService.userRole = '';
+              this.router.navigate(['welcome']);
+            }).catch((error: any) => {
+              alert("注意：请不要在校外登陆系统。 IP地址:" + ipdata.ip);
+              this._sharedService.userName = '';
+              this._sharedService.userRole = '';
+              this.router.navigate(['welcome']);
+            });
+        }
+      });
       this._sharedService.userName = data.userName === undefined ? '' : data.userName;
       this._sharedService.userRole = data.role === undefined ? '' : data.role;
       console.log("login successful: user:" + this._sharedService.userName + " role:" + this._sharedService.userRole);
